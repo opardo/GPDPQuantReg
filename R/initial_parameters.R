ec771e78bb05ed1a32b3ed05e53b51abaeed38dd
Pardo Gomez Carlos Omar <MB75168@cb.bbvabancomer.com.mx>
2017-07-16 01:00:07 -0500

    Change M to support matrixes.

--- R/initial_parameters.R
+++ R/initial_parameters.R
@@ -2,7 +2,7 @@ get_quant_p_from_Y_function <- function(formula, data, p){
   mf <- model.frame(formula = formula, data = data)
   Y <- scale(model.response(data = mf))
   Q <- as.numeric(quantile(Y, p))
-  quant_p <- function(x) rep(Q, length(x))
+  quant_p <- function(X) rep(Q, nrow(X))
   return(quant_p)
 }
 
